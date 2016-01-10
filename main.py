bd62406964a847795ebcbf21cb06514cf47b4c08
Anthony Smith <asmith@cari.net>
2016-01-10 13:14:25 -0800

    Changed around functions to include db_con and removed 'main' function.

--- main.py
+++ main.py
@@ -4,24 +4,26 @@ import mysql_con, roll_calc
 import locale
 locale.setlocale(locale.LC_ALL, '')
 
-def wh_info(db_con):
-	c = db_con
-	sys = raw_input("System: ")
-	result = c.query(sys)
+def wh_info(wh_code):
+	c = mysql_con.crit_db()
+	wh_code = wh_code
+	result = c.query(wh_code)
 	try:
-		print("\nName: " + str(result[0][0]) + "\n"
+		x = ("\nName: " + str(result[0][0]) + "\n"
 			"Leads to: " + str(result[0][1]) + "\n"
 			"Max Stable Time: " + str(result[0][2]) + " Hours\n"
 			"Max Mass: " + locale.format("%d", result[0][3], 1) + " kg\n"
 			"Mass Regen: " + locale.format("%d", result[0][4], 1) + " Per Cycle\n"
 			"Max Jump Mass: " + locale.format("%d", result[0][5], 1) + " kg\n")
-	except IndexError:
-		print("Invalid WH code.")
-		main()
-	d = roll_calc.mass_calc(float(result[0][3]))
-	d.get_mass()
+		x = "<br />".join(x.split("\n"))
+		return x
 
-def main():
-	db_con = mysql_con.crit_db()
-	wh_info(db_con)
-main()
+	except IndexError:
+		return "Invalid WH code."
+		# main()
+def wh_calc(wh_code, params):
+	c = mysql_con.crit_db()
+	wh_code = wh_code
+	result = c.query(wh_code)
+	d = roll_calc.mass_calc(float(result[0][3]), params)
+	return d.start_pass()
